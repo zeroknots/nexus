d0800c3f9b90c49de5841578f8c67abbfb149b8d
zeroknots <zeroknots@protonmail.com>
2024-11-20 10:33:56 +0700

    done

--- scripts/DeployNexus.s.sol
+++ scripts/DeployNexus.s.sol
@@ -9,7 +9,6 @@ import "forge-std/console2.sol";
 abstract contract DeployNexus is Script {
     function deploy(uint256 pKey) public {
         bytes32 salt = 0x00000000000000000000000000000000000000006c22c2c2bc572b01b3ff9c91;
-        salt = 0x00000000000000000000000000000000000000006c22c2c2bc572b01b3ff9c91;
         bytes memory bytecode = vm.getCode("./artifacts/Nexus/Nexus.json");
         bytes memory args = abi.encode(address(0x0000000071727De22E5E9d8BAf0edAc6f37da032));
         address smartSession = SafeSingletonDeployer.broadcastDeploy(pKey, bytecode, args, salt);
